ec2637a9e8ffdda3a017fb7f4010e32db7f7a3dd
Laís Cruz <133046134+laiscrz@users.noreply.github.com>
2024-10-02 13:21:18 -0300

    chore: create script.sql

---
+++ script.sql
@@ -0,0 +1,9 @@
+/*
+
+Bianca Leticia Román Caldeira - RM552267 - Turma : 2TDSPH
+Charlene Aparecida Estevam Mendes Fialho - RM552252 - Turma : 2TDSPH
+Lais Alves Da Silva Cruz - RM:552258 - Turma : 2TDSPH
+
+CP5 - Banco de Dados - Tema: Posto de Gasolina
+
+*/
